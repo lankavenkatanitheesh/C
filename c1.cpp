20f7ceb2de014ff06bc5bfc2ceb1bfe870339bcc
Lanka venkata Nitheesh <lankavenkatanitheesh@gmail.com>
2026-07-24 13:57:21 +0530

    Add files via upload

---
+++ c1.cpp
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+  int main()
+ 	{
+ 	int	a,b,c,d,sum;
+ 	float average;
+ 	printf("enter value of a,b,c,d=");
+ 	scanf("%d%d%d%d",&a,&b,&c,&d);
+ 	sum=a+b+c+d;
+ 	average=sum/4;
+ 	
+ 	printf("average%f",average);
+ 	printf("sum%d",sum);
+ 	
+	 }
